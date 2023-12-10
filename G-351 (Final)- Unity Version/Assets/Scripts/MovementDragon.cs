987222cf0ecc9b8e6ef2b7a3d303791ba2fea6a5
Dycoles <colesdylan12@gmail.com>
2023-12-09 18:32:41 -0700

    Move, lighting, animations

--- G-351 (Final)- Unity Version/Assets/Scripts/MovementDragon.cs
+++ G-351 (Final)- Unity Version/Assets/Scripts/MovementDragon.cs
@@ -4,34 +4,114 @@ using UnityEngine;
 
 public class MovementDragon : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
-    
+    public float impulseForce = 170000.0f;
+    public float impulseTorque = 3000.0f;
+    public float shootingCooldown = 1f;
+    private float lastShotTime;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    Animator animController;
+    Rigidbody rigidBody;
+    AudioSource audioSource; // Added AudioSource
+
+    // Reference to the AudioSource component for gunshot sound
+    AudioSource gunshotAudioSource;
+
+    // Reference to the AudioSource component for default background music
+    public AudioSource defaultMusicAudioSource; // Drag your default music AudioSource from the Inspector
+
+    // Reference to the AudioSource component for fight track
+    public AudioSource fightTrackAudioSource; // Drag your fight track AudioSource from the Inspector
+
+    // Start is called before the first frame update
     void Start()
     {
+        animController = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>(); // Get AudioSource component
 
+        // Get the AudioSource component for gunshot sound
+        gunshotAudioSource = GetComponent<AudioSource>();
+
+        // Set the initial background music
+        if (defaultMusicAudioSource != null)
+        {
+            if (!defaultMusicAudioSource.isPlaying)
+            {
+                defaultMusicAudioSource.Play();
+            }
+
+            // Stop the fight track if it's playing
+            if (fightTrackAudioSource != null && fightTrackAudioSource.isPlaying)
+            {
+                fightTrackAudioSource.Stop();
+            }
+        }
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(0, 0, speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(0, 0, -speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(speed * Time.deltaTime, 0, 0);
-        }
-        float mouseX = Input.GetAxis("Mouse X");
-        transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0);
+        Vector3 input = new Vector3(0, Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
+        if (input.magnitude > 0.001 && !animController.GetBool("Crouch"))
+        {
+            rigidBody.AddRelativeTorque(new Vector3(0, input.y * impulseTorque * Time.deltaTime, 0));
+            rigidBody.AddRelativeForce(new Vector3(0, 0, input.z * impulseForce * Time.deltaTime));
+
+            animController.SetBool("Move", true);
+        }
+        else
+        {
+            animController.SetBool("Move", false);
+
+            if (Input.GetKey(KeyCode.F) && Time.time - lastShotTime >= shootingCooldown)
+            {
+                Shoot();
+                lastShotTime = Time.time;
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        // Instantiate the bullet
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        // Play gunshot sound
+        PlayGunshotSound();
+
+        // Start the fight track if not already playing
+        StartFightTrack();
+    }
+
+    void PlayGunshotSound()
+    {
+        // Check if the AudioSource component for gunshot sound exists
+        if (gunshotAudioSource != null)
+        {
+            // Play the gunshot sound
+            gunshotAudioSource.Play();
+        }
+    }
+
+    void StartFightTrack()
+    {
+        // Check if the AudioSource component for fight track exists
+        if (fightTrackAudioSource != null)
+        {
+            // Stop the default music if it's playing
+            if (defaultMusicAudioSource != null && defaultMusicAudioSource.isPlaying)
+            {
+                defaultMusicAudioSource.Stop();
+            }
+
+            // Play the fight track if it's not already playing
+            if (!fightTrackAudioSource.isPlaying)
+            {
+                fightTrackAudioSource.Play();
+            }
+        }
     }
 }
