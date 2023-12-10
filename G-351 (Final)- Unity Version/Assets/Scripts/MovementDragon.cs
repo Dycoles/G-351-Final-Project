0f414c5e4593f20a96e10ed1d93bf3de7933ba04
Dycoles <colesdylan12@gmail.com>
2023-12-09 17:22:05 -0700

    botched movement, someone fix pls

--- G-351 (Final)- Unity Version/Assets/Scripts/MovementDragon.cs
+++ G-351 (Final)- Unity Version/Assets/Scripts/MovementDragon.cs
@@ -1,33 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementDragon : MonoBehaviour
 {
     public float speed;
-    public float turning;
+    public float rotationSpeed;
+    
+    void Start()
+    {
 
-    // Reference to the camera GameObject
-    public GameObject cameraObject;
+    }
 
     void Update()
     {
-        // Player movement logic
         if (Input.GetKey(KeyCode.W))
-            transform.Translate(0, 0, speed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.S))
-            transform.Translate(0, 0, -speed * Time.deltaTime / 2);
-        if (Input.GetKey(KeyCode.A))
-            transform.Rotate(0, -turning * Time.deltaTime, 0);
-        if (Input.GetKey(KeyCode.D))
-            transform.Rotate(0, turning * Time.deltaTime, 0);
-
-        // Camera follow logic
-        if (cameraObject != null)
         {
-            // Set the camera position to follow the player
-            cameraObject.transform.position = new Vector3(transform.position.x, transform.position.y + 2f, transform.position.z - 5f);
-
-            // Look at the player from the back
-            cameraObject.transform.LookAt(transform.position);
+            transform.Translate(0, 0, speed * Time.deltaTime);
         }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        float mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0);
+
     }
 }
