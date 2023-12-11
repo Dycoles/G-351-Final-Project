248aff0feddff20b5035811db776841d631d94c6
jennifergg1 <galvanjennifer001@gmail.com>
2023-12-10 19:14:29 -0700

    corgi steps timed

--- G-351 (Final)- Unity Version/Assets/Scripts/MovementDragon.cs
+++ G-351 (Final)- Unity Version/Assets/Scripts/MovementDragon.cs
@@ -6,106 +6,79 @@ public class MovementDragon : MonoBehaviour
 {
     public float impulseForce = 170000.0f;
     public float impulseTorque = 3000.0f;
-    public float shootingCooldown = 1f;
-    private float lastShotTime;
-    public GameObject bulletPrefab;
-    public Transform firePoint;
 
     Animator animController;
     Rigidbody rigidBody;
-    AudioSource audioSource; // Added AudioSource
+    AudioSource audioSource;
 
-    // Reference to the AudioSource component for gunshot sound
-    AudioSource gunshotAudioSource;
+    public AudioClip footstepSound; 
 
-    // Reference to the AudioSource component for default background music
-    public AudioSource defaultMusicAudioSource; // Drag your default music AudioSource from the Inspector
-
-    // Reference to the AudioSource component for fight track
-    public AudioSource fightTrackAudioSource; // Drag your fight track AudioSource from the Inspector
+    // Variable to check if the corgi is moving
+    private bool isMoving = false;
 
     // Start is called before the first frame update
     void Start()
     {
         animController = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>(); // Get AudioSource component
-
-        // Get the AudioSource component for gunshot sound
-        gunshotAudioSource = GetComponent<AudioSource>();
-
-        // Set the initial background music
-        if (defaultMusicAudioSource != null)
-        {
-            if (!defaultMusicAudioSource.isPlaying)
-            {
-                defaultMusicAudioSource.Play();
-            }
-
-            // Stop the fight track if it's playing
-            if (fightTrackAudioSource != null && fightTrackAudioSource.isPlaying)
-            {
-                fightTrackAudioSource.Stop();
-            }
-        }
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 input = new Vector3(0, Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isCurrentlyMoving = input.magnitude > 0.001;
 
-        if (input.magnitude > 0.001)
+        if (isCurrentlyMoving)
         {
             rigidBody.AddRelativeTorque(new Vector3(0, input.y * impulseTorque * Time.deltaTime, 0));
             rigidBody.AddRelativeForce(new Vector3(0, 0, input.z * impulseForce * Time.deltaTime));
 
             animController.SetBool("Move", true);
+
+            // Play footstep sound only when the corgi starts moving
+            if (!isMoving)
+            {
+                PlayFootstepSound();
+            }
+
+            // Set the corgi as moving
+            isMoving = true;
         }
         else
         {
             animController.SetBool("Move", false);
+
+            // Stop the footstep sound when the corgi stops moving
+            if (isMoving)
+            {
+                StopFootstepSound();
+            }
+
+            // Set the corgi as not moving
+            isMoving = false;
         }
     }
 
-    void Shoot()
+    void PlayFootstepSound()
     {
-        // Instantiate the bullet
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-
-        // Play gunshot sound
-        PlayGunshotSound();
-
-        // Start the fight track if not already playing
-        StartFightTrack();
-    }
-
-    void PlayGunshotSound()
-    {
-        // Check if the AudioSource component for gunshot sound exists
-        if (gunshotAudioSource != null)
+        // Check if the AudioSource component and footstep sound exist
+        if (audioSource != null && footstepSound != null && !audioSource.isPlaying)
         {
-            // Play the gunshot sound
-            gunshotAudioSource.Play();
+            // Set the footstep sound to the AudioSource
+            audioSource.clip = footstepSound;
+            // Play the footstep sound
+            audioSource.Play();
         }
     }
 
-    void StartFightTrack()
+    void StopFootstepSound()
     {
-        // Check if the AudioSource component for fight track exists
-        if (fightTrackAudioSource != null)
+        // Stop the footstep sound if it is currently playing
+        if (audioSource != null && audioSource.isPlaying)
         {
-            // Stop the default music if it's playing
-            if (defaultMusicAudioSource != null && defaultMusicAudioSource.isPlaying)
-            {
-                defaultMusicAudioSource.Stop();
-            }
-
-            // Play the fight track if it's not already playing
-            if (!fightTrackAudioSource.isPlaying)
-            {
-                fightTrackAudioSource.Play();
-            }
+            audioSource.Stop();
         }
     }
 }
